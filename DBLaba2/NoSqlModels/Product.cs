d918ce2157f927faab3b0d370ab3c577f47d9f2e
DartGys <boda20043@gmail.com>
2023-12-03 22:32:31 +0200

    done

---
+++ DBLaba2/NoSqlModels/Product.cs
@@ -0,0 +1,28 @@
+﻿using DBLaba2.SqlModels;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace DBLaba2.NoSqlModels
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string? ProductName { get; set; }
+
+        public string? Description { get; set; }
+
+        public decimal? Price { get; set; }
+
+        public DateOnly? RegistrationDate { get; set; }
+
+        public byte[] LastModifiedDate { get; set; } = null!;
+
+        public int? ModifiedBy { get; set; }
+
+        public virtual ICollection<Inventory> Inventories { get; set; } = new List<Inventory>();
+
+        public virtual ICollection<SqlModels.SpecialOffer> SpecialOffers { get; set; } = new List<SqlModels.SpecialOffer>();
+    }
+}
